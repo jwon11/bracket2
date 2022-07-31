3035adec12584200271897e20f3de9b639f38995
sourbeatles3 <85785497+sourbeatles3@users.noreply.github.com>
2022-07-31 14:26:43 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 // 변수 선언 시작
+
+// resoft news 변수 선언
 const contents = document.querySelectorAll('.content');
 const content = [];
 for(let i = 0; i < contents.length; i++) {
@@ -16,16 +18,47 @@ const content_p = [];
 for(let i = 0; i < content_ps.length; i++) {
     content_p.push(content_ps[i]);
 }
+// resoft news 변수 선언 끝
 
+// resoft project 변수 선언
 const proj_ecoce = document.querySelector('.proj_ecoce');
 const ecoce_word = document.querySelector('.ecoce_word_wrap');
 const ecoce_img = document.querySelector('.ecoce_img');
 const proj_moado = document.querySelector('.proj_moado');
 const moado_word = document.querySelector('.moado_word_wrap');
 const moado_img = document.querySelector('.moado_img');
+// resoft project 변수 선언 끝
+
+// resoft service 변수 선언
+const serv_app = document.querySelector('.serv_content_app');
+const serv_web = document.querySelector('.serv_content_web');
+const serv_ai = document.querySelector('.serv_content_ai');
+const serv_xr = document.querySelector('.serv_content_xr');
+
+const serv_content_imgs = document.querySelectorAll('.serv_content_img');
+const serv_content_img = [];
+for(let i = 0; i < serv_content_imgs.length; i++) {
+    serv_content_img.push(serv_content_imgs[i]);
+}
+
+const content_fronts = document.querySelectorAll('.content_front');
+const content_front = [];
+for(let i = 0; i < content_fronts.length; i++) {
+    content_front.push(content_fronts[i]);
+}
+
+const content_behinds = document.querySelectorAll('.content_behind');
+const content_behind = [];
+for(let i = 0; i < content_behinds.length; i++) {
+    content_behind.push(content_behinds[i]);
+}
+// resoft service 변수 선언 끝
+
 // 변수 선언 끝
 
+
 // 함수 선언 시작
+
 // resoft news content mouseover & mouseout 기능
 const content_function = function() {
     for(let i = 0; i < 4; i++) {
@@ -73,10 +106,71 @@ const proj_moado_function = function() {
         moado_img.style.transform = 'scale(1.0)';
     });
 }
+
+// resoft service 이미지 mouseover & mouseout 기능
+const serv_function = function() {
+    serv_app.addEventListener('mouseover', (e) => {
+        e.preventDefault();
+        serv_content_img[0].style.transform = 'scale(1.2)';
+        content_front[0].style.display = 'none';
+        content_behind[0].style.display = 'table';
+    });
+
+    serv_app.addEventListener('mouseout', (e) => {
+        e.preventDefault();
+        serv_content_img[0].style.transform = 'scale(1.0)';
+        content_front[0].style.display = 'table';
+        content_behind[0].style.display = 'none';
+    });
+
+    serv_web.addEventListener('mouseover', (e) => {
+        e.preventDefault();
+        serv_content_img[1].style.transform = 'scale(1.2)';
+        content_front[1].style.display = 'none';
+        content_behind[1].style.display = 'table';
+    });
+
+    serv_web.addEventListener('mouseout', (e) => {
+        e.preventDefault();
+        serv_content_img[1].style.transform = 'scale(1.0)';
+        content_front[1].style.display = 'table';
+        content_behind[1].style.display = 'none';
+    });
+
+    serv_ai.addEventListener('mouseover', (e) => {
+        e.preventDefault();
+        serv_content_img[2].style.transform = 'scale(1.2)';
+        content_front[2].style.display = 'none';
+        content_behind[2].style.display = 'table';
+    });
+
+    serv_ai.addEventListener('mouseout', (e) => {
+        e.preventDefault();
+        serv_content_img[2].style.transform = 'scale(1.0)';
+        content_front[2].style.display = 'table';
+        content_behind[2].style.display = 'none';
+    });
+
+    serv_xr.addEventListener('mouseover', (e) => {
+        e.preventDefault();
+        serv_content_img[3].style.transform = 'scale(1.2)';
+        content_front[3].style.display = 'none';
+        content_behind[3].style.display = 'table';
+    });
+
+    serv_xr.addEventListener('mouseout', (e) => {
+        e.preventDefault();
+        serv_content_img[3].style.transform = 'scale(1.0)';
+        content_front[3].style.display = 'table';
+        content_behind[3].style.display = 'none';
+    });
+}
 // 함수 선언 끝
+
 
 // 함수 실행 시작
 content_function();
 proj_ecoce_function();
 proj_moado_function();
+serv_function();
 // 함수 실행 끝
