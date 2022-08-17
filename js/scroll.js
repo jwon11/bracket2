88ead48c0fbed7b12039da13b2a4911c4ec3e931
InGyeong <103246753+igit322@users.noreply.github.com>
2022-08-17 17:05:05 +0900

    Add files via upload

---
+++ js/scroll.js
@@ -0,0 +1,16 @@
+window.addEventListener("scroll", function () {
+  let menu = window.pageYOffset;
+  if (menu > 830) {
+    $(".nav_wrap").addClass("add");
+    $(".logo_wrap img").addClass("add");
+    $(".toggle_wrap").addClass("add");
+    $("nav a:link, nav a:visited").addClass("add");
+    $("nav a:hover, nav a:active").addClass("add");
+  } else {
+    $(".nav_wrap").removeClass("add");
+    $(".logo_wrap img").removeClass("add");
+    $(".toggle_wrap").removeClass("add");
+    $("nav a:link, nav a:visited").removeClass("add");
+    $("nav a:hover, nav a:active").removeClass("add");
+  }
+});
